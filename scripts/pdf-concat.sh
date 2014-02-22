95e08bfc7a238fa2df125306c65598be06506e9b
Quoc-Viet Nguyen <afelion@gmail.com>
2014-02-22 18:25:45 +1000

    Add script concatenating pdf

---
+++ scripts/pdf-concat.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ $# -lt 1 ]; then
+	echo "$0 <OUTPUT_FILE> <INPUT_FILES...>"
+	exit 1
+fi
+OUTPUT_FILE="$1"
+PAPER_SIZE=a4
+
+shift 1
+
+gs -sDEVICE=pdfwrite -sPAPERSIZE="$PAPERSIZE"\
+	-dNOPAUSE -dBATCH -dSAFER \
+	-sOutputFile="$OUTPUT_FILE" \
+	"$@"
--- scripts/pdf-extract.sh
+++ scripts/pdf-extract.sh
@@ -3,19 +3,20 @@
 # $3 Last page
 # Output file: "InputFile_pXX-pYY.pdf"
 
-FIRST_PAGE=$2
-if [ -z $1 ] || [ -z $FIRST_PAGE ]; then
+INPUT_FILE="$1"
+FIRST_PAGE="$2"
+if [ -z "$INPUT_FILE" ] || [ -z "$FIRST_PAGE" ]; then
 	echo "$0 <PDF_FILE> <FIRST_PAGE> [LAST_PAGE]"
 	exit 1
 fi
 
-LAST_PAGE=$3
-if [ -z $LAST_PAGE ]; then
-	LAST_PAGE=$FIRST_PAGE
+LAST_PAGE="$3"
+if [ -z "$LAST_PAGE" ]; then
+	LAST_PAGE="$FIRST_PAGE"
 fi
 
 gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER \
-	-dFirstPage=${FIRST_PAGE} \
-	-dLastPage=${LAST_PAGE} \
-	-sOutputFile=${1%.pdf}_p${FIRST_PAGE}-p${LAST_PAGE}.pdf \
-	${1}
+	-dFirstPage="$FIRST_PAGE" \
+	-dLastPage="$LAST_PAGE" \
+	-sOutputFile="${1%.pdf}_p${FIRST_PAGE}-p${LAST_PAGE}.pdf" \
+	"$INPUT_FILE"
