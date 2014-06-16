2d6d5df3ff4039f21e97fdd2800e280b554cd1f2
Quoc-Viet Nguyen <afelion@gmail.com>
2014-06-16 23:36:57 +1000

    vim: pathogen

---
+++ vim/fetch-repo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir -p ~/.vim/bundle
+cd ~/.vim/bundle
+
+git clone https://github.com/tpope/vim-pathogen
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/vim-scripts/taglist.vim
