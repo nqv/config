183b2dbb01d40bf20aeead279c82a2cc80e6e753
Quoc-Viet Nguyen <afelion@gmail.com>
2014-11-03 14:51:01 +1000

    vim: shallow clone only

--- vim/fetch-repo.sh
+++ vim/fetch-repo.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
+git_clone() {
+  repo=`basename $1`
+  echo "Checking $repo..."
+  if [ -d "$repo" ]; then
+    (cd "$repo" && git pull)
+  else
+    git clone --depth 1 $1
+  fi
+}
+
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 
-git clone https://github.com/tpope/vim-pathogen
-git clone https://github.com/scrooloose/nerdtree
-git clone https://github.com/scrooloose/nerdcommenter.git
-git clone https://github.com/majutsushi/tagbar
-git clone https://github.com/fatih/vim-go
+git_clone https://github.com/tpope/vim-pathogen
+git_clone https://github.com/scrooloose/nerdtree
+git_clone https://github.com/scrooloose/nerdcommenter
+git_clone https://github.com/majutsushi/tagbar
+git_clone https://github.com/fatih/vim-go
