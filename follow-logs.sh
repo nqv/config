744d93c992dcf17ed135a68ef3c80cbd2001d7e6
Quoc-Viet Nguyen <afelion@gmail.com>
2015-03-04 12:43:32 +1000

    Script following a log file in different servers

---
+++ follow-logs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+LOGFILE=$1
+shift
+SERVERS=$@
+
+if [ -z "$LOGFILE" ] || [ -z "$SERVERS" ]; then
+	echo $0 "<LOGFILE> <SERVER>..."
+	exit 1
+fi
+
+COUNT=0
+tmux new-window -n "$LOGFILE"
+
+for SERVER in $SERVERS; do
+	NAME="$SERVER:$LOGFILE"
+
+	CMD="ssh $SERVER tail -F $LOGFILE"
+
+	if [[ $COUNT -eq 0 ]]; then
+		tmux send-keys "$CMD" C-m
+	else
+		tmux split-window "$CMD" \; select-layout tiled
+	fi
+	COUNT=$((COUNT + 1))
+done
