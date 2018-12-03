573b7978b5cd65aeb11ef6e97be19263d5ae5d25
Quoc-Viet Nguyen <afelion@gmail.com>
2018-12-03 23:34:09 +1000

    Status script needs bash

--- status.sh
+++ status.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 CPU=""
 CPUIDLE=0
 CPUTOTAL=0
 cpu() {
 	# cpu 2:user 3:nice 4:system 5:idle 6:iowait 7:irq 8:softirq 9:steal 10:guest 11:guest_nice
-	read CPUIDLE CPUTOTAL CPU <<< "$(grep 'cpu ' /proc/stat | awk -v pidle="$CPUIDLE" -v ptotal="$CPUTOTAL" '{idle=$5+$6; total=$2+$3+$4+$5+$6+$7+$8+$9+$10+$11; if (ptotal>0) {percent=int(100*(total-ptotal-idle+pidle)/(total-ptotal))} else {percent=0}; print idle,total,percent}')"
+	read CPUIDLE CPUTOTAL CPU < <(grep 'cpu ' /proc/stat | awk -v pidle="$CPUIDLE" -v ptotal="$CPUTOTAL" '{idle=$5+$6; total=$2+$3+$4+$5+$6+$7+$8+$9+$10+$11; if (ptotal>0) {percent=int(100*(total-ptotal-idle+pidle)/(total-ptotal))} else {percent=0}; print idle,total,percent}')
 }
 
 MEMORY=""
@@ -16,12 +16,17 @@ memory() {
 BATTERY=""
 BATTERYPATH="/sys/class/power_supply/BAT0"
 battery() {
-	PRESENT="$(cat "$BATTERYPATH/present")"
-	if [ "$PRESENT" != "1" ]; then
-		BATTERY="--"
+	BATTERY="--"
+	PRESENT="$BATTERYPATH/present"
+	STATUS="$BATTERYPATH/status"
+	if [ ! -f "$PRESENT" ] || [ ! -f "$STATUS" ]; then
 		return
 	fi
-	STATUS="$(cat "$BATTERYPATH/status")"
+	PRESENT="$(cat "$PRESENT")"
+	if [ "$PRESENT" != "1" ]; then
+		return
+	fi
+	STATUS="$(cat "$STATUS")"
 	case "$STATUS" in
 		Full)
 			BATTERY="100"
